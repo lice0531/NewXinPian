2eaec500355cfceeb70707bb937594aa5d0b93e2
lice0531 <450098886@qq.com>
2022-06-15 12:07:54 +0800

    1.扫码添加28位格式

--- app/src/main/java/android_serialport_api/xingbang/firingdevice/ReisterMainPage_scan.java
+++ app/src/main/java/android_serialport_api/xingbang/firingdevice/ReisterMainPage_scan.java
@@ -330,6 +330,14 @@ public class ReisterMainPage_scan extends SerialPortActivity implements LoaderCa
                     barCode = data.substring(0, 13);
                     denatorId = data.substring(13,26);
                     insertSingleDenator_2(barCode, denatorId,data.substring(26));//同时注册管壳码和芯片码
+                }else if (data.length() == 28) {
+                    //5620302H00001A62F400FFF20AB603
+                    //5420302H00001A6F4FFF20AB603
+                    //Y5620413H00009A630FD74D87604
+                    barCode = data.substring(1, 14);
+                    String a = data.substring(14, 24);
+                    denatorId = a.substring(0, 2) + "2" + a.substring(2, 4) + "00" + a.substring(4);
+                    insertSingleDenator_2(barCode, denatorId, data.substring(24));//同时注册管壳码和芯片码
                 } else {
                     barCode = getContinueScanBlastNo(data);//VR:1;SC:5600508H09974;
                     insertSingleDenator(barCode);
