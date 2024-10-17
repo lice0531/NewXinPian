3b03413b33dd3b981cb8bf5a2da41ff78e5ea6ba
lice0531 <450098886@qq.com>
2024-10-17 14:57:40 +0800

    界面调整

--- app/src/main/java/android_serialport_api/xingbang/firingdevice/ReisterMainPage_line.java
+++ app/src/main/java/android_serialport_api/xingbang/firingdevice/ReisterMainPage_line.java
@@ -549,7 +549,7 @@ public class ReisterMainPage_line extends SerialPortActivity implements LoaderCa
                         Utils.writeRecord("--起爆测试--:总线短路");
                         closeThread();
                         AlertDialog dialog = new AlertDialog.Builder(ReisterMainPage_line.this)
-                                .setTitle("芯片短路")//设置对话框的标题//"成功起爆"
+                                .setTitle("线路短路")//设置对话框的标题//"成功起爆"
                                 .setMessage("电压过低，检查铆接线路是否正常")//设置对话框的内容"本次任务成功起爆！"
                                 //设置对话框的按钮
                                 .setNeutralButton("退出", (dialog12, which) -> {
@@ -570,7 +570,7 @@ public class ReisterMainPage_line extends SerialPortActivity implements LoaderCa
                         Utils.writeRecord("--起爆测试--:总线短路");
                         closeThread();
                         AlertDialog dialog = new AlertDialog.Builder(ReisterMainPage_line.this)
-                                .setTitle("芯片短路")//设置对话框的标题//"成功起爆"
+                                .setTitle("线路短路")//设置对话框的标题//"成功起爆"
                                 .setMessage("电流过大，检查铆接线路是否正常")//设置对话框的内容"本次任务成功起爆！"
                                 //设置对话框的按钮
                                 .setNeutralButton("退出", (dialog12, which) -> {
@@ -624,7 +624,7 @@ public class ReisterMainPage_line extends SerialPortActivity implements LoaderCa
             }
             if (tipInfoFlag == 10) {//断路
                 AlertDialog dialog = new AlertDialog.Builder(ReisterMainPage_line.this)
-                        .setTitle("芯片断路")//设置对话框的标题//"成功起爆"
+                        .setTitle("线路断路")//设置对话框的标题//"成功起爆"
                         .setMessage("电流为0，请检查铆接线路是否正常")//设置对话框的内容"本次任务成功起爆！"
                         //设置对话框的按钮
                         .setNeutralButton("退出", (dialog12, which) -> {
