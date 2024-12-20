7d6c96e750dfc80a54b2b346aab127c2f037e243
lice0531 <450098886@qq.com>
2024-12-20 18:25:56 +0800

    1.设置时间5D

--- app/src/main/java/android_serialport_api/xingbang/cmd/DefCommand.java
+++ app/src/main/java/android_serialport_api/xingbang/cmd/DefCommand.java
@@ -49,7 +49,8 @@ public class DefCommand {
 
 	public static String CMD_5_TEST_8="5B";//设置低压
 	public static String CMD_5_TEST_9="5C";//设置高压
-	public static String CMD_5_TEST_10="5E";//设置时间参数
+	public static String CMD_5_TEST_10="5D";//设置时间参数
+	public static String CMD_5_TEST_11="5E";//设置时间参数
 
 	
 	
