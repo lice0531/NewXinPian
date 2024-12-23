027df2a4eb790c36b0dc576269a0a610e0c4c92f
lice0531 <450098886@qq.com>
2024-12-23 14:06:18 +0800

    1.区域

---
+++ app/src/main/java/android_serialport_api/xingbang/firingdevice/QuYuActivity.java
@@ -0,0 +1,16 @@
+package android_serialport_api.xingbang.firingdevice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android_serialport_api.xingbang.R;
+
+public class QuYuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qu_yu);
+    }
+}
