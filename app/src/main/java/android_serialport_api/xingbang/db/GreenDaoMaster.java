ae8cd6e9896c86723df45ab2b62dbe337b4c1d77
lice0531 <450098886@qq.com>
2025-02-14 14:25:35 +0800

    1.只有雷管正常才更新雷管状态

--- app/src/main/java/android_serialport_api/xingbang/db/GreenDaoMaster.java
+++ app/src/main/java/android_serialport_api/xingbang/db/GreenDaoMaster.java
@@ -1569,7 +1569,7 @@ public class GreenDaoMaster {
                 .build()
                 .unique();
         Log.e("更新生产库中的起爆状态", "shell: "+shell );
-        if(entity!=null&&!entity.equals("雷管正常")&&!entity.equals("雷管已使用")){
+        if(entity!=null&&entity.equals("雷管正常")){
             Log.e("更新生产库中的起爆状态", "entity: "+entity.toString() );
             entity.setQibao(qibao);
             detonatorTypeNewDao.update(entity);
